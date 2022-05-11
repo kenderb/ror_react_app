6a942d94b98ff829bc93a5282bc417282026c1c9
KBolivar <kbolivar@pibox.app>
2022-05-11 14:26:32 -0500

    Change Book name to title.

---
+++ db/migrate/20220511192254_rename_book_name_column.rb
@@ -0,0 +1,5 @@
+class RenameBookNameColumn < ActiveRecord::Migration[5.2]
+  def change
+    rename_column :books, :name, :title
+  end
+end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
   describe 'validations' do
-    it { should validate_presence_of(:name) }
-    it { should validate_uniqueness_of(:name) }
+    it { should validate_presence_of(:title) }
+    it { should validate_uniqueness_of(:title) }
     it { should validate_presence_of(:content) }
   end
 
