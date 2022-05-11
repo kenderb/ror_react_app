8ccfd17447e0fac46bb95c27f2307a9f59b6f3de
KBolivar <kbolivar@pibox.app>
2022-05-11 13:05:48 -0500

    Add Course model.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,8 @@
 FactoryGirl.define do
+  factory :course do
+    name 'Course name'
+  end
+
   factory :user do |f|
     f.name 'Kender'
     f.email 'Kender@mail.com'
--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Course, :type => :model do
+RSpec.describe Course, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:name) }
   end
