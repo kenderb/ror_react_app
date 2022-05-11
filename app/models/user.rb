e4910fa2ece5b25a822ee4e91c7ad27bf73589bd
KBolivar <kbolivar@pibox.app>
2022-05-11 16:30:49 -0500

    Add email validation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP } 
 
   has_many :students, class_name: :User, foreign_key: :instructor_id
   belongs_to :instructor, class_name: :User, optional: true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
+  let(:course) { create(:course) }
+  let(:user) { build(:user, course: course, email: 'Some random bad email') }
+
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email) }
     it { should define_enum_for(:role).with(%i[student instructor]) }
+
+    it 'should not accept a invalid email format' do
+      expect(user.valid?).to be false
+    end
   end
 
   describe 'associations' do
